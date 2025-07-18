943aed698a17325ecd22bf2d4b03a73014d2c700
Pedro de Leon <pedro.deleon@epicor.com>
2025-07-18 10:15:17 -0600

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/invitacion-boda/' : '/',
+  base: '/',
   server: {
     host: "0.0.0.0",
     port: 8080,
