d00551d5900dc2a5dd51dc87479c89ba5ceedba9
Pedro de Leon <pedro.deleon@epicor.com>
2025-07-18 08:09:46 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/invitacion-boda/',
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 }));
-
-
-
